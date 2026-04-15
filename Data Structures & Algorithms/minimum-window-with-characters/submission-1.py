897d4a5f0272240a1b1a7f222843ebf68b83e21c
Gary Sun <garysun15@gmail.com>
2026-04-14 22:05:18 -0600

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,61 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        t_freq = {}
+        for c in t:
+            t_freq[c] = t_freq.get(c, 0) + 1
+
+        s_freq = {}
+        l = 0
+        shortest = None
+        need = len(t_freq)  # unique chars needed
+        have = 0
+
+        for r in range(len(s)):
+            s_freq[s[r]] = s_freq.get(s[r], 0) + 1
+            if s[r] in t_freq and s_freq[s[r]] == t_freq[s[r]]:
+                have += 1  # just hit the exact count needed for this char
+
+            while have == need:
+                if shortest is None or (r - l + 1) < len(shortest):
+                    shortest = s[l:r+1]
+                s_freq[s[l]] -= 1
+                if s[l] in t_freq and s_freq[s[l]] < t_freq[s[l]]:
+                    have -= 1  # lost a required char
+                l += 1
+
+        return shortest if shortest is not None else ""
+        
+        # t_freq = {}
+        # for c in t:
+        #     t_freq[c] = t_freq.get(c, 0) + 1
+
+        # s_freq = {}
+        # l = 0
+        # shortest = None
+
+        # def valid():
+        #     for key in t_freq:
+        #         if s_freq.get(key, 0) < t_freq[key]:
+        #             return False
+        #     return True
+
+        # for r in range(len(s)):
+        #     s_freq[s[r]] = s_freq.get(s[r], 0) + 1
+
+
+        #     while valid():
+        #         if shortest is None or len(s[l:r+1]) < len(shortest):
+        #             shortest = s[l:r+1]
+
+        #         if s_freq[s[l]] == 1:
+        #             del s_freq[s[l]]
+        #         else:
+        #             s_freq[s[l]] -= 1
+        #         l += 1
+        
+        # return shortest if shortest is not None else ""
+
+
+        
+
